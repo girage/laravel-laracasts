f6fb44b0fb453d33b730559aa841e309c86cc6e8
Trust <45748977+girage@users.noreply.github.com>
2022-11-25 03:50:41 +0700

    done ch.25

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,10 +5,11 @@
         <h1>
             {{ $post->title }}
         </h1>
-
+        <p>
+            <a href="/categories/{{ $post->category->slug}}"> {{ $post->category->name }}</a>
+        </p>
         <div>
-            {{-- <?= $post->body ?> --}}
-            {!! $post->body !!}
+            {{ $post->body }}
         </div>
     </article>
     <a href="/">Go Back</a>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,10 +21,14 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('posts/{post}', function ($slug) {
-
+Route::get('posts/{post}', function ($id) {
     return view('post', [
-        'post' =>  Post::findOrFail($slug)
+        'post' =>  Post::findOrFail($id)
     ]);
-})->where('post', '[A-z_\-]+');
-// })->whereAlpha('post');
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
+    ]);
+});
